a0716c8098c7a7bbd5052966f721c1fef445451d
Antonio Placido <aplacido1992@gmail.com>
2020-07-17 22:51:16 -0400

    4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,19 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 // getDadJoke
 
-// 4.1
-// getJoke().then((data) => console.log(data));
+const getDadJoke = new Promise((resolve, reject) => {
+  try {
+    const joke = request({
+      uri: "https://icanhazdadjoke.com/",
+      header: { Accept: "application/json" },
+      json: true,
+    }).then((response) => {
+      resolve(response.joke);
+    });
+  } catch (err) {
+    reject(err);
+  }
+});
+
+getDadJoke.then((data) => console.log(data));
--- handlers.js
+++ handlers.js
@@ -1,1 +1,16 @@
 // nothing here yet...
+
+// getDadJoke
+
+function getDadJoke(req, res) {
+  const joke = req.params.joke;
+  res.status(200).json({
+    status: 200,
+    Accept: "application/json",
+    joke,
+  });
+  return joke;
+  console.log(joke);
+}
+
+module.exports = { getDadJoke };
